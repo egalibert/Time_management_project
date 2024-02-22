75ee191921663b1347b96f7fbc37c458808019ac
Elliot <elliotgalibert@gmail.com>
2024-02-22 10:02:04 +0200

    added comments to functions

--- report_handler.py
+++ report_handler.py
@@ -6,6 +6,7 @@ from extra_table_creation import get_average_hours_per_day_per_consultant
 
 con = None
 
+# Queries all information from working_hours table
 def all_rows():
     try:
         con = psycopg2.connect(**config())
@@ -22,15 +23,18 @@ def all_rows():
             con.close()
     return(row)
 
+# Changes the seconds (from using datetime) into hours
 def timedelta_to_hours(td):
     return td.total_seconds() / 3600.0
 
+# Calculates the proper values and writes consultant_hours, customer_hours and cumulative_customer_hours and average hours into a file
 def write_to_file(data):
     consultant_hours = defaultdict(timedelta)
     customer_hours = defaultdict(timedelta)
     cumulative_customer_hours = defaultdict(timedelta)
     CustomerRecord = namedtuple('CustomerRecord', ['customer_name', 'total_hours'])
 
+    # takes the passed data (working_hours) and splits it into variables and calculates the needed information
     for record in data:
         record_id = record[0]
         start_time = datetime.strptime(str(record[1]), '%Y-%m-%d %H:%M:%S')
@@ -44,6 +48,7 @@ def write_to_file(data):
         customer_hours[(consultant_name, customer_name)] += total_working_time
         cumulative_customer_hours[customer_name] += total_working_time
 
+    # opens report file and writes information based on calculations
     with open('working_hours_report.txt', 'w') as file:
         file.write("Consultant Report:\n")
         for consultant, total_hours in consultant_hours.items():
@@ -66,7 +71,7 @@ def write_to_file(data):
             print("No data retrieved from get_average_hours_per_day_per_consultant")
 
 
-
+# main function runs the program
 def main():
     data = all_rows()
     write_to_file(data)
@@ -74,3 +79,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
